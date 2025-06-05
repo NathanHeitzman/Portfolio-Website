ce1970efba76cdc0b932ad588824014fb41de58b
NathanHeitzman <nathanheitzman3500@gmail.com>
2025-06-05 16:33:15 -0500

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './assets/styles/App.css'
 import NavBar from './components/NavBar'
 import Footer from './components/Footer'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import HeadShot from './assets/images/nathan2.jpg'
+import Home from './pages/Home'
+
 
 function App() {
   return(
     <>
-    <NavBar />
-      <div className='page-body d-flex justify-content-center align-items-center'>
-        <div className='row w-100 container'>
-          <div className='image-column col-12 col-md-6 order-md-2'>
-            <img src={HeadShot} className='my-headshot img-fluid'></img>
-          </div>
-          
-          <div className='text-column col-12 col-md-6 order-md-1'>
-              <h1 className='mb-0'>Hey there!</h1>
-              <h2>I'm <span>Nathan Heitzman</span></h2>
-              <h2>A Computer Science Student</h2>
-              <button>Lets Build Something Together!</button>
-          </div>
-        </div>
-      </div>
-    <Footer />
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />}> </Route>
+      </Routes>
+    </Router>
     </>
   )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,28 @@
 import Navbar from "../components/NavBar"
 import Footer from "../components/Footer"
 import 'bootstrap/dist/css/bootstrap.min.css'
+import HeadShot from '../assets/images/nathan2.jpg'
 
 const Homepage = () => {
     return(
       <>
         <Navbar />
-
+          <div className='page-body d-flex justify-content-center align-items-center'>
+            <div className='row w-100 container'>
+              <div className='image-column col-12 col-md-6 order-md-2'>
+                <img src={HeadShot} className='my-headshot img-fluid'></img>
+              </div>
+  
+              <div className='text-column col-12 col-md-6 order-md-1'>
+                <h1 className='mb-0'>Hey there!</h1>
+                <h2>I'm <span>Nathan Heitzman</span></h2>
+                <h2>A Computer Science Student</h2>
+                <button>Lets Build Something Together!</button>
+              </div>
+            </div>
+          </div>
         <Footer />
       </>
     )
 }
+export default Homepage;
