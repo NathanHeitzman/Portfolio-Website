6c0792fd369ae22416bdd09011f62e26c9e521a0
NathanHeitzman <nathanheitzman3500@gmail.com>
2025-06-05 23:13:38 -0500

    formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './assets/styles/App.css'
-import Home from './pages/Home'
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/styles/App.css";
+import Home from "./pages/Home";
 
 function App() {
-  return(
+  return (
     <>
-    <Router>
-      <Routes>
-        <Route path='/' element={<Home />}> </Route>
-      </Routes>
-    </Router>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,12 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import '../assets/styles/button.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../assets/styles/button.css";
 
 interface ButtonProps {
-    text: string;
-    onClick?: () => void;
+  text: string;
+  onClick?: () => void;
 }
 
-const Button = ({text, onClick}: ButtonProps) => {
-    return(
-        <button className='about-btn'>{text}</button>
-    )
-}
+const Button = ({ text }: ButtonProps) => {
+  return <button className="about-btn">{text}</button>;
+};
 export default Button;
