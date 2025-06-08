570aef4f46e26498b5d67708b529ecf21eaf1cc6
NathanHeitzman <nathanheitzman3500@gmail.com>
2025-06-08 01:46:48 -0500

    added html for contact page, started work on styles

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,22 +1,29 @@
-import { useNavigate, type NavigateFunction } from "react-router-dom"
+import { useNavigate, type NavigateFunction } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../assets/styles/button.css";
 
 interface ButtonProps {
   text: string;
   onClick?: () => void;
+  className?: string;
 }
 
 function clicked(text: string, navigate: NavigateFunction) {
-  if(text === "Lets Build Something Together!"){
+  if (text === "Lets Build Something Together!") {
     navigate("/contact-me");
+  } else if (text === "Submit") {
+    //TODO, add email functionality
+    console.log("Email Sent!");
   }
 }
 
-const Button = ({ text }: ButtonProps) => {
+const Button = ({ text, className }: ButtonProps) => {
   const navigate = useNavigate();
   return (
-    <button className="about-btn" onClick={() => clicked(text,navigate)}>
+    <button
+      className={`about-btn ${className ?? ""}`} //if className is null append nothing
+      onClick={() => clicked(text, navigate)}
+    >
       {text}
     </button>
   );
--- src/pages/ContactMe.tsx
+++ src/pages/ContactMe.tsx
@@ -1,13 +1,49 @@
 import "../assets/styles/ContactMe.css";
 import Navbar from "../components/NavBar";
 import Footer from "../components/Footer";
+import Button from "../components/Button"
+import Handshake from "../assets/images/handshake.png"
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const ContactMe = () => {
   return (
     <>
       <Navbar />
-      <h1>CONTACT ME</h1>
+      <div className=" contactMe-page container-fluid row">
+        <div className="contentRight col-12 col-md-6 order-md-2">
+          <div className="inputs-container d-flex flex-column justify-content-center">
+            <input className="user-input" type="text"/>
+            <input className="user-input" type="email"/>
+            <input className="user-comments-input" type="text"/>
+            <Button className="align-self end" text="Submit"/>
+          </div>
+        </div>
+
+        <div className="contentLeft col-12 col-md-6 order-md-1">
+          <div className="reasons-container">
+            <div className="reasons-text">
+              <ul>
+                <p>Message me to:</p>
+                <li className="contact-list-item">
+                  Reason 1
+                </li>
+                <li className="contact-list-item">
+                  Longer Reason 2
+                </li>
+                <li className="contact-list-item">
+                  Reason 3
+                </li>
+                <li className="contact-list-item">
+                  Super super long reason 4
+                </li>
+              </ul>
+            </div>
+            <div className="reasons-image-container">
+              <img className="reasons-image" src={Handshake} alt="An image of me shaking hands with a co-worker"></img>
+            </div>
+          </div>
+        </div>
+      </div>
       <Footer />
     </>
   );
