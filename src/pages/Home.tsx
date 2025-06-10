643a1efc7fa0aca47fb5bcf0a78b79567bc46c1f
NathanHeitzman <nathanheitzman3500@gmail.com>
2025-06-10 01:47:36 -0500

    added 12 icons for testing

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,31 +1,68 @@
-import Navbar from "../components/NavBar"
-import Footer from "../components/Footer"
-import 'bootstrap/dist/css/bootstrap.min.css'
-import HeadShot from '../assets/images/nathan2.jpg'
-import Button from '../components/Button'
-import '../assets/styles/Home.css'
+import Navbar from "../components/NavBar";
+import Footer from "../components/Footer";
+import "bootstrap/dist/css/bootstrap.min.css";
+import HeadShot from "../assets/images/nathan2.jpg";
+import Button from "../components/Button";
+import "../assets/styles/Home.css";
+
+//Icons
+import Icon1 from "../assets/images/icons/typescript.png";
+import Icon2 from "../assets/images/icons/react.png";
+import Icon3 from "../assets/images/icons/python.png";
+import Icon4 from "../assets/images/icons/javascript.png";
+import Icon5 from "../assets/images/icons/java.png";
+import Icon6 from "../assets/images/icons/html.png";
+import Icon7 from "../assets/images/icons/github.png";
+import Icon8 from "../assets/images/icons/git.png";
+import Icon9 from "../assets/images/icons/excel.png";
+import Icon10 from "../assets/images/icons/css.png";
+import Icon11 from "../assets/images/icons/c.png";
+import Icon12 from "../assets/images/icons/bootstrap.png";
 
 const Homepage = () => {
-    return(
-      <>
-        <Navbar />
-          <div className='page-body d-flex justify-content-center align-items-center'>
-            <div className='row w-100 container'>
-              <div className='image-column col-12 col-md-6 order-md-2'>
-                <img src={HeadShot} className='my-headshot img-fluid'></img>
-              </div>
-  
-              <div className='text-column col-12 col-md-6 order-md-1'>
-                <h1 className='mb-0'>Hey there!</h1>
-                <h2>I'm <span className="gradient-name">Nathan Heitzman</span></h2>
-                <h2>A Computer Science Student</h2>
-                <br></br>
-                <Button text="Lets Build Something Together!" />
-              </div>
-            </div>
+  return (
+    <>
+      <Navbar />
+      <div className="page-body d-flex justify-content-center align-items-center">
+        <div className="row w-100 container">
+          <div className="image-column col-12 col-md-6 order-md-2">
+            <img src={HeadShot} className="my-headshot img-fluid"></img>
           </div>
-        <Footer />
-      </>
-    )
-}
+
+          <div className="text-column col-12 col-md-6 order-md-1">
+            <h1 className="mb-0">Hey there!</h1>
+            <h2>
+              I'm <span className="gradient-name">Nathan Heitzman</span>
+            </h2>
+            <h2>A Computer Science Student</h2>
+            <br></br>
+            <Button text="Lets Build Something Together!" />
+          </div>
+        </div>
+      </div>
+
+      <div className="tech-skills page-body d-flex justify-content-center align-items-center">
+        {" "}
+        test
+        <div className="row w-100 container">
+          <div className="icons col-sm-4 col-lg-8 col-xxl-12">
+            <img className="icon-image" src={Icon1} alt="" />
+            <img className="icon-image" src={Icon2} alt="" />
+            <img className="icon-image" src={Icon3} alt="" />
+            <img className="icon-image" src={Icon4} alt="" />
+            <img className="icon-image" src={Icon5} alt="" />
+            <img className="icon-image" src={Icon6} alt="" />
+            <img className="icon-image" src={Icon7} alt="" />
+            <img className="icon-image" src={Icon8} alt="" />
+            <img className="icon-image" src={Icon9} alt="" />
+            <img className="icon-image" src={Icon10} alt="" />
+            <img className="icon-image" src={Icon11} alt="" />
+            <img className="icon-image" src={Icon12} alt="" />
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
 export default Homepage;
