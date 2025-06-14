15c4460b1f6aa591dd65b1b8419cf54c2be33464
NathanHeitzman <nathanheitzman3500@gmail.com>
2025-06-13 19:37:47 -0500

    added project page skeleton / images

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,11 +1,23 @@
 import Navbar from "../components/NavBar";
 import Footer from "../components/Footer";
+import Project1 from "../assets/images/project.png"
+import "../assets/styles/projects.css"
 
 const Projects = () => {
     return(
         <>
         <Navbar />
-            <h1>MY PROJECTS</h1>
+            <div className="projects-container container-fluid d-flex flex-column align-items-center">
+                <div className="img-container">
+                    <img className="project-image" src={Project1} alt="project 1"/>
+                </div>
+                <div className="img-container">
+                    <img className="project-image" src={Project1} alt="project 2" />
+                </div>
+                <div className="img-container">
+                    <img className="project-image" src={Project1} alt="project 3" />
+                </div>
+            </div>
         <Footer />
         </>
     )
