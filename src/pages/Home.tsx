21039925971cd37e96660d8fde5641ab730febf0
NathanHeitzman <nathanheitzman3500@gmail.com>
2025-06-14 23:17:31 -0500

    organized icons

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,23 +6,25 @@ import Button from "../components/Button";
 import "../assets/styles/Home.css";
 
 //Icons
-import Icon1 from "../assets/images/icons/typescript.png";
-import Icon2 from "../assets/images/icons/react.png";
-import Icon3 from "../assets/images/icons/python.png";
-import Icon4 from "../assets/images/icons/javascript.png";
-import Icon5 from "../assets/images/icons/java.png";
-import Icon6 from "../assets/images/icons/html.png";
-import Icon7 from "../assets/images/icons/github.png";
-import Icon8 from "../assets/images/icons/git.png";
-import Icon9 from "../assets/images/icons/excel.png";
-import Icon10 from "../assets/images/icons/css.png";
-import Icon11 from "../assets/images/icons/c.png";
-import Icon12 from "../assets/images/icons/bootstrap.png";
-import Icon13 from "../assets/images/icons/figma.png";
+
+
+import Icon1 from "../assets/images/icons/python.png";
+import Icon2 from "../assets/images/icons/java.png";
+import Icon3 from "../assets/images/icons/c.png";
+import Icon4 from "../assets/images/icons/html.png";
+import Icon5 from "../assets/images/icons/css.png";
+import Icon6 from "../assets/images/icons/javascript.png";
+import Icon7 from "../assets/images/icons/typescript.png";
+import Icon8 from "../assets/images/icons/react.png";
+import Icon9 from "../assets/images/icons/bootstrap.png";
+import Icon10 from "../assets/images/icons/git.png";
+import Icon11 from "../assets/images/icons/github.png";
+import Icon12 from "../assets/images/icons/figma.png";
+import Icon13 from "../assets/images/icons/excel.png";
 import Icon14 from "../assets/images/icons/word.png";
 import Icon15 from "../assets/images/icons/powerpoint.png";
-import Icon16 from "../assets/images/icons/docker.png";
-import Icon17 from "../assets/images/icons/teams.png";
+import Icon16 from "../assets/images/icons/teams.png";
+import Icon17 from "../assets/images/icons/docker.png";
 import Icon18 from "../assets/images/icons/ubuntu.png";
 import Icon19 from "../assets/images/icons/windows.png";
 import Icon20 from "../assets/images/icons/sql.png";
