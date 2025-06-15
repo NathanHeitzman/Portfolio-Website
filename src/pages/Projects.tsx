e3c155c26bc1825acaf5ff655889bc8ae1485bda
NathanHeitzman <nathanheitzman3500@gmail.com>
2025-06-14 23:29:26 -0500

    added text box outlines and some template contents

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,25 +1,61 @@
 import Navbar from "../components/NavBar";
 import Footer from "../components/Footer";
-import Project1 from "../assets/images/project.png"
-import "../assets/styles/projects.css"
+import Project1 from "../assets/images/project.png";
+import "../assets/styles/projects.css";
 
 const Projects = () => {
-    return(
-        <>
-        <Navbar />
-            <div className="projects-container container-fluid d-flex flex-column align-items-center">
-                <div className="img-container">
-                    <img className="project-image" src={Project1} alt="project 1"/>
-                </div>
-                <div className="img-container">
-                    <img className="project-image" src={Project1} alt="project 2" />
-                </div>
-                <div className="img-container">
-                    <img className="project-image" src={Project1} alt="project 3" />
-                </div>
-            </div>
-        <Footer />
-        </>
-    )
-}
+  return (
+    <>
+      <Navbar />
+      <div className="projects-container container-fluid d-flex flex-column align-items-center">
+        <div className="img-container">
+          <img className="project-image" src={Project1} alt="project 1" />
+        </div>
+        <div className="text-container d-flex flex-column">
+          <h3 className="align-self-center mt-3">Project 1</h3>
+          <p className="align-self-center p-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At,
+            similique voluptas iure dolorum ab quidem aliquid rem quod
+            voluptatem qui. Impedit, quae ab excepturi qui porro soluta
+            voluptatibus sequi adipisci! Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Laudantium vitae commodi reprehenderit natus autem
+            dolor at deserunt neque officiis! Deleniti, perspiciatis similique!
+            Corrupti unde, molestiae quaerat doloremque dolor nesciunt quod.
+          </p>
+        </div>
+        <div className="img-container">
+          <img className="project-image" src={Project1} alt="project 2" />
+                  <div className="text-container d-flex flex-column">
+          <h3 className="align-self-center mt-3">Project 1</h3>
+          <p className="align-self-center p-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At,
+            similique voluptas iure dolorum ab quidem aliquid rem quod
+            voluptatem qui. Impedit, quae ab excepturi qui porro soluta
+            voluptatibus sequi adipisci! Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Laudantium vitae commodi reprehenderit natus autem
+            dolor at deserunt neque officiis! Deleniti, perspiciatis similique!
+            Corrupti unde, molestiae quaerat doloremque dolor nesciunt quod.
+          </p>
+        </div>
+        </div>
+        <div className="img-container">
+          <img className="project-image" src={Project1} alt="project 3" />
+                  <div className="text-container d-flex flex-column">
+          <h3 className="align-self-center mt-3">Project 1</h3>
+          <p className="align-self-center p-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At,
+            similique voluptas iure dolorum ab quidem aliquid rem quod
+            voluptatem qui. Impedit, quae ab excepturi qui porro soluta
+            voluptatibus sequi adipisci! Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Laudantium vitae commodi reprehenderit natus autem
+            dolor at deserunt neque officiis! Deleniti, perspiciatis similique!
+            Corrupti unde, molestiae quaerat doloremque dolor nesciunt quod.
+          </p>
+        </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
 export default Projects;
