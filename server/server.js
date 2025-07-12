5eb114d2254b03991b31b9779679def1649ecba6
NathanHeitzman <nathanheitzman3500@gmail.com>
2025-07-12 13:06:58 -0500

    added emailjs public key

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
-require("dotenv").config({path: "../.env"}); //loads environment variables
+require("dotenv").config(); //loads environment variables, make sure to run this script from the portfolio-website folder
 
 const express = require("express");
 const app = express();
-const port = 8080;
+const port = process.env.BACKEND_PORT || 8080; //fall back to port 8080 if port is not specified
 
 const cors = require("cors");
 const corsOptions = {
