2bcca4920522ebf3b75436cd65a7f3b8de86ba10
NathanHeitzman <nathanheitzman3500@gmail.com>
2025-07-12 12:42:47 -0500

    added setup for environment variables

--- server/server.js
+++ server/server.js
@@ -1,5 +1,9 @@
+require("dotenv").config({path: "../.env"}); //loads environment variables
+
 const express = require("express");
 const app = express();
+const port = 8080;
+
 const cors = require("cors");
 const corsOptions = {
     origin: ["http://localhost:5173"], //requests only accepted from this origin
@@ -11,6 +15,6 @@ app.get("/api", (req,res) => {
     res.json({"tests" : ["test1","test2","test3"]});
 });
 
-app.listen(8080, () => {
-    console.log("Server started on port 8080");
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/styles/Home.css";
