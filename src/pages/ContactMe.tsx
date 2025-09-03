4b263a7c04616a18d2f3a4593fd4af5367d4a611
NathanHeitzman <nathanheitzman3500@gmail.com>
2025-09-03 13:07:48 -0500

    modularized contact page

--- src/pages/ContactMe.tsx
+++ src/pages/ContactMe.tsx
@@ -1,4 +1,4 @@
-import "../assets/styles/ContactMe.css";
+import styles from "../assets/styles/ContactMe.module.css";
 import Navbar from "../components/NavBar";
 import Footer from "../components/Footer";
 import Button from "../components/Button";
@@ -39,45 +39,45 @@ const ContactMe = () => {
       <Navbar />
       <div className="contact-page mb-5">
       <div className="container-fluid row align-items-stretch">
-        <div className="content-right col-12 col-md-6 order-md-2">
-          <div className="inputs-container d-flex justify-content-center align-items-center">
-            <div className="inner-input-container d-flex flex-column">
+        <div className={`${styles.contentRight} col-12 col-md-6 order-md-2`}>
+          <div className={`${styles.inputsContainer} d-flex justify-content-center align-items-center`}>
+            <div className= {`${styles.innerInputContainer} d-flex flex-column`}>
               <form
                 ref={form}
                 onSubmit={sendEmail}
-                className="inner-input-container d-flex flex-column"
+                  className={`${styles.innerInputContainer} d-flex flex-column`}
               >
                 <input
-                  className="user-input"
+                  className = {`${styles.userInput}`}
                   name="user-name"
                   placeholder="Username:"
                   type="text"
                 />
                 <input
-                  className="user-input"
+                  className= {`${styles.userInput}`}
                   name="user-email-address"
                   placeholder="Email Address:"
                   type="email"
                 />
                 <input
-                  className="user-input"
+                  className= {`${styles.userInput}`}
                   name="user-phone-number"
                   placeholder="Phone Number:"
                   type="text"
                 />
                 <input 
-                className="user-input"
+                className={`${styles.userInput}`}
                 name="user-website"
                 placeholder="Company Website URL:"
                 type="url" 
                 />
                 <textarea
-                  className="user-comments-input"
+                  className={`${styles.userCommentsInput}`}
                   name="user-message"
                   placeholder="Comments:"
                 />
-                <div className="button-container d-flex justify-content-center justify-content-lg-end">
-                  <Button className="submit-button align-self-end py-2 px-4" text="Submit" />
+                <div className={`${styles.buttonContainer} d-flex justify-content-center justify-content-lg-end`}>
+                  <Button className={`${styles.buttonContainer} align-self-end py-2 px-4`} text="Submit" />
                 </div>
               </form>
             </div>
@@ -86,18 +86,18 @@ const ContactMe = () => {
 
         <div className="contentLeft col-12 col-md-6 order-md-1">
           <div className="reasons-container">
-            <div className="reasons-text">
+            <div className={`${styles.reasonsText}`}>
               <p>Message me to inquire about:</p>
               <ul>
-                <li className="contact-list-item">Project collaboration</li>
-                <li className="contact-list-item">Job / Internship opportunities</li>
-                <li className="contact-list-item">Provide feedback on a project</li>
-                <li className="contact-list-item">Connect and talk</li>
+                <li className={`${styles.contactListItem}`}>Project collaboration</li>
+                  <li className={`${styles.contactListItem}`}>Job / Internship opportunities</li>
+                  <li className={`${styles.contactListItem}`}>Provide feedback on a project</li>
+                  <li className={`${styles.contactListItem}`}>Connect and talk</li>
               </ul>
             </div>
             <div className="reasons-image-container">
               <img
-                className="reasons-image"
+                className={`${styles.reasonsImage}`}
                 src={Handshake}
                 alt="An image of me shaking hands with a co-worker"
               ></img>
