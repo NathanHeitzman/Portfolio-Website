e99f1daa912d3668c53676826f888a18e4ffd7ac
NathanHeitzman <nathanheitzman3500@gmail.com>
2026-04-12 23:21:19 -0500

    completed project page refactoring

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -24,57 +24,49 @@ const Projects = () => {
                 <ProjectCard
                     projectImage={SongScraping}
                     projectTitle="Metadata Scraping / Automated Song Backup"
-                    projectDescription="This is a description"
+                    projectDescription={description1}
                     projectTechIcons={[PythonIcon,SeleniumIcon]}
                 />
-        
-                <div className={`${styles.imageContainer}`}>
-                    <img className={`${styles.projectImage}`} src={CsvPaymentConfirmation} alt="project 2" />
-                    <div className={`${styles.textContainer} d-flex flex-column`}>
-                        <h3 className="align-self-center mt-3">Automated Payment Verification</h3>
-                        <p className="align-self-center p-4">
-                        I created a custom Python script to automate the verification of product payments 
-                        by comparing two CSV files—one containing customer order data and the other listing 
-                        Venmo transaction history. This project significantly streamlined what was previously 
-                        a manual and error-prone task by automatically cross-referencing customer names, 
-                        amounts, and timestamps to confirm completed payments. Built and tested in VS Code, the 
-                        script reduced data entry and verification time by over 90%, greatly improving both speed 
-                        and accuracy in processing customer transactions.
-                        </p>
-                        <div className="d-flex flex-row align-items-center">
-                            <p className="px-4">Technologies Used:</p>
-                            <img className={`${styles.icon}`} src={PythonIcon} />
-                        </div>
-                    </div>
-                </div>
-                <div className={`${styles.imageContainer}`}>
-                    <img className={`${styles.projectImage}`} src={PortfolioWebsite} alt="project 3" />
-                    <div className={`${styles.textContainer} d-flex flex-column`}>
-                        <h3 className="align-self-center mt-3">This Website</h3>
-                        <p className="align-self-center p-4">
-                        I designed and developed my own personal portfolio website from scratch as a way 
-                        to represent my skills, creativity, and growth as a developer. Every part of the 
-                        site—from the layout to the interactive elements—was built by me. I started by creating 
-                        the full design in Figma, then brought it to life using HTML, CSS, JavaScript, and 
-                        TypeScript. I used React for component-based structure and Bootstrap to ensure the 
-                        site is fully responsive and visually consistent across all devices. This project is 
-                        more than just a portfolio—it's a custom-built platform that reflects who I am as a developer, 
-                        built entirely with tools and technologies I chose and implemented myself.
-                        </p>
-                        <div className="d-flex flex-row align-items-center">
-                            <p className="px-4">Technologies Used:</p>
-                            <img className={`${styles.icon}`} src={HTML} />
-                            <img className={`${styles.icon}`} src={CSS} />
-                            <img className={`${styles.icon}`} src={TypeScript} />
-                            <img className={`${styles.icon}`} src={React} />
-                            <img className={`${styles.icon}`} src={Figma} />
-                            <img className={`${styles.icon}`} src={Bootstrap} />
-                        </div>
-                    </div>
-                </div>
+                <div className="my-3"></div>
+                <ProjectCard
+                    projectImage={CsvPaymentConfirmation}
+                    projectTitle="Automated Payment Verification"
+                    projectDescription={description2}
+                    projectTechIcons={[PythonIcon]}
+                />
+                <div className="my-3"></div>
+                <ProjectCard
+                    projectImage={PortfolioWebsite}
+                    projectTitle="This Website"
+                    projectDescription={description3}
+                    projectTechIcons={[HTML,CSS,TypeScript,React,Figma,Bootstrap]}
+                />
             </div>
             <Footer />
         </>
     );
 };
 export default Projects;
+
+const description1 = ` I built a two-part Python automation pipeline to extract song titles and artist names 
+                        from a web-based playlist. I used Selenium to scrape the data and save it to a structured text file. 
+                        Then, I wrote a second script that automatically searched each track on YouTube, captured the top 
+                        video URL, and compiled the links into a list for batch downloading. I used this list with yt-dlp to 
+                        efficiently download the entire playlist, streamlining the process of creating offline music libraries.`;
+
+const description2 = `  I created a custom Python script to automate the verification of product payments 
+                        by comparing two CSV files—one containing customer order data and the other listing 
+                        Venmo transaction history. This project significantly streamlined what was previously 
+                        a manual and error-prone task by automatically cross-referencing customer names, 
+                        amounts, and timestamps to confirm completed payments. Built and tested in VS Code, the 
+                        script reduced data entry and verification time by over 90%, greatly improving both speed 
+                        and accuracy in processing customer transactions.`;
+
+const description3 = `  I designed and developed my own personal portfolio website from scratch as a way 
+                        to represent my skills, creativity, and growth as a developer. Every part of the 
+                        site—from the layout to the interactive elements—was built by me. I started by creating 
+                        the full design in Figma, then brought it to life using HTML, CSS, JavaScript, and 
+                        TypeScript. I used React for component-based structure and Bootstrap to ensure the 
+                        site is fully responsive and visually consistent across all devices. This project is 
+                        more than just a portfolio—it's a custom-built platform that reflects who I am as a developer, 
+                        built entirely with tools and technologies I chose and implemented myself.`;
